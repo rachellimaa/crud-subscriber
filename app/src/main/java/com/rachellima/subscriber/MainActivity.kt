6c684f14670728f70d2ef22c7fe62959e3b1b6d7
Rachel Lima <rachellimaa@gmail.com>
2021-02-04 15:57:11 -0400

    Add view binding and remove deprecated code
    Signed-off-by: Rachel Lima <rachellimaa@gmail.com>

--- app/src/main/java/com/rachellima/subscriber/MainActivity.kt
+++ app/src/main/java/com/rachellima/subscriber/MainActivity.kt
@@ -5,17 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
-import kotlinx.android.synthetic.main.activity_main.*
+import com.rachellima.subscriber.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(navController.graph)
-        app_toolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.appToolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
